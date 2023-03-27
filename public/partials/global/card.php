360d004e613198210436067453aad1a7c84444bb
Wood3001 <wood3001@gmail.com>
2023-03-26 23:06:41 -0700

    card images clickable

--- public/partials/global/card.php
+++ public/partials/global/card.php
@@ -1,6 +1,8 @@
 <div class="card flex flex-column blur">
     <div class="card__image">
-        <img src="<?php echo h($artifact['img_url']); ?>" alt="Preview image for my '<?php echo h($artifact['title']); ?>' project">
+        <a href="<?php echo h($artifact['link_url']); ?>">
+            <img src="<?php echo h($artifact['img_url']); ?>" alt="Preview image for my '<?php echo h($artifact['title']); ?>' project">
+        </a>
     </div>
     <div class="card__message">
         <h3><?php echo h($artifact['date_posted']); ?> | <?php echo $artifact['discipline'] == 0 ? 'Web Development' : 'Design'; ?></h3>
