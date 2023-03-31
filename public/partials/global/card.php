d9cb809fc6c62fb4cab195263daed7116d0c3f6c
Wood3001 <wood3001@gmail.com>
2023-03-31 14:31:19 -0700

    project card updates

--- public/partials/global/card.php
+++ public/partials/global/card.php
@@ -8,9 +8,5 @@
         <h3><?php echo h($artifact['date_posted']); ?> | <?php echo $artifact['discipline'] == 0 ? 'Web Development' : 'Design'; ?></h3>
         <h2><?php echo h($artifact['title']); ?></h2>
         <p><?php echo h($artifact['description']); ?></p>
-        
-    </div>
-    <div class="card__cta flex">
-        <a href="<?php echo h($artifact['link_url']); ?>"><button class="btn-1 btn-flattop" aria-label="View Project Details"><h3>View Project Details</h3></button></a>
     </div>
 </div>
