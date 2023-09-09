de36e633e743fb5362165d72ea94b6f2396ffd74
Wood3001 <wood3001@gmail.com>
2023-09-08 17:56:17 -0700

    artifact updates

--- public/partials/global/artifact.php
+++ public/partials/global/artifact.php
@@ -13,7 +13,7 @@
                 <div class="artifact__specs art-anim" id="specs">
                     <div id="image">
                         <img src="<?php echo h($artifact['img_url']); ?>" alt="Preview image for my '<?php echo h($artifact['title']); ?>' project">
-                        <a href="<?php echo ($artifact['nar_link']); ?>" target="_blank" aria-label="View The Project"><button class="btn-1"><h3>View The Project</h3></button></a>
+                        <a href="<?php echo ($artifact['nar_link']); ?>" target="_blank" aria-label="View The Project"><button class="btn-1"><h3><?php echo ($artifact['button_text']); ?></h3></button></a>
                     </div>
                     <div class="specs-wrapper">
                         <div id="tech">
